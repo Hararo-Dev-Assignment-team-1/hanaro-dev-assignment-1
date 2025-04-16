db36da2a529cd96bce968df907c85b00d929f94b
VarGun <heegun46900@gmail.com>
2025-04-16 15:18:54 +0900

    feat: header component 개발

---
+++ js/header/header-scroll.js
@@ -0,0 +1,18 @@
+// js/header/header-scroll.js
+document.addEventListener("DOMContentLoaded", () => {
+  const headerWrapper =
+    document
+      .querySelector("header-component")
+      ?.shadowRoot?.querySelector(".header-wrapper") ||
+    document.querySelector(".header-wrapper");
+
+  const onScroll = () => {
+    if (window.scrollY > 0) {
+      headerWrapper.classList.add("scrolled");
+    } else {
+      headerWrapper.classList.remove("scrolled");
+    }
+  };
+
+  window.addEventListener("scroll", onScroll);
+});
